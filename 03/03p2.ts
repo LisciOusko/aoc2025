5e6dbb18c5953232ef313b547f6cb796dc53bfc0
Josef <josef.cerpl@aimtecglobal.com>
2025-12-03 21:59:08 +0100

    day three, part two

---
+++ 03/03p2.ts
@@ -0,0 +1,45 @@
+import * as fs from 'fs';
+
+function readInput() {
+    return fs.readFileSync("03/input.txt", "utf-8");
+}
+
+function main() {
+    const lines = readInput().split('\n');
+    let sum = 0;
+
+    for (const line of lines) {
+        const highest = getJoltage(line);
+        sum += highest;
+    }
+
+    console.log(sum);
+}
+
+function getJoltage(line: string): number {
+    let highest = 0;
+
+    const visitor = (numStr: string, substr: string, layer: number) => {
+        let checked: string[] = [];
+
+        if (!layer) {
+            highest = parseInt(numStr);
+            return;
+        }
+
+        for (let i = 0; i < substr.length; i++) {
+            const char = substr[i];
+            if (checked.includes(char)
+            || parseInt(String(highest).slice(0, numStr.length + 1)) > parseInt(numStr + substr[i])) continue;
+
+            checked.push(char);
+            visitor(numStr + substr[i], substr.slice(i + 1), layer - 1);
+        }
+    }   
+
+    visitor("", line, 12);
+    return highest;
+}
+
+
+main();
